2dc03a5b6ea5237815c48e836a688c4449f57218
j33wong <j33wong@gmail.com>
2022-01-03 23:36:39 -0500

    added test for unauthenticated attempt at getting a post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,9 +1,23 @@
-
+from app import schemas
 
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
 
+    # map the posts in the response
+    def validate(post):
+        return schemas.PostOut(**post)
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+
 
 
