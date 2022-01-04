7e1dad24dd2e104ff477b9b8cb92d6d5d32b4b4d
j33wong <j33wong@gmail.com>
2022-01-04 01:13:06 -0500

    created votes test and voting twice on a post

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -23,7 +23,7 @@ def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = 
 
     if (vote.dir == 1):
         if found_vote:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} has already voted on post {vote.post_it}")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} has already voted on post {vote.post_id}")
         new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id)
         db.add(new_vote)
         db.commit()
