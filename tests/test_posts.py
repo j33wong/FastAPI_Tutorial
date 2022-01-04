92e14c391e10b2f96dd9bd9d2cf1a8fde95d2c88
j33wong <j33wong@gmail.com>
2022-01-03 23:37:57 -0500

    added test for unauthorized for get single post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -17,7 +17,9 @@ def test_unauthorized_user_get_all_posts(client, test_posts):
     res = client.get("/posts/")
     assert res.status_code == 401
 
-
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
 
 
 
